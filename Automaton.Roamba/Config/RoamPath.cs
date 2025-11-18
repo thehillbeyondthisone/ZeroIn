7d9f51c364e5749c71141257e7ca6b6c0d274215
thehillbeyondthisone <thehillbeyondthisone@gmail.com>
2025-11-17 20:19:06 -0600

    Add files via upload

---
+++ Automaton.Roamba/Config/RoamPath.cs
@@ -0,0 +1,50 @@
+﻿using AOSharp.Pathfinding;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace AutomatonRoamba
+{
+    public class RoamPath
+    {
+        public SPath SPath;
+        public TargetingRules Rules;
+
+        public RoamPath()
+        {
+            SPath = SPath.Create();
+            Rules = new TargetingRules();
+        }
+
+        public static RoamPath Load(string path)
+        {
+            RoamPath roamPath;
+
+            try
+            {
+                roamPath = JsonConvert.DeserializeObject<RoamPath>(File.ReadAllText(path));
+            }
+            catch
+            {
+                roamPath = new RoamPath();
+            }
+
+            roamPath.SPath.IsLocked = true;
+
+            return roamPath;
+        }
+
+        public void Save(string savePath)
+        {
+            try
+            {
+                File.WriteAllText(savePath, JsonConvert.SerializeObject(this, Formatting.Indented));
+                AutomatonRoamba.Log.Information($"File saved at '{savePath}'");
+            }
+            catch (Exception ex)
+            {
+                AutomatonRoamba.Log.Warning(ex.Message);
+            }
+        }
+    }
+}
---
+++ Automaton.Roamba/StateMachine/FSM.cs
@@ -0,0 +1,90 @@
+﻿using AOSharp.Core.Misc;
+using AOSharp.Core;
+using AOSharp.Pathfinding;
+using Stateless;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Remoting.Contexts;
+using AOSharp.Core.UI;
+
+namespace AutomatonRoamba
+{
+    public interface IState
+    {
+        void Tick();
+        void OnStateEnter();
+        void OnStateExit();
+    }
+
+    public class FSMProvider<TState, TTrigger, TContext>
+    {
+        public readonly FSM<TState, TTrigger, TContext> StateMachine;
+
+        public FSMProvider(FSM<TState, TTrigger, TContext> stateMachine)
+        {
+            StateMachine = stateMachine;
+        }
+    }
+
+    public abstract class FSM<TState, TTrigger, TContext> : StateMachine<TState, TTrigger>
+    {
+        public TContext Context;
+        private IState _executingState = null;
+        private Dictionary<TState, Type> _states = new Dictionary<TState, Type>();
+        private bool _initialized = false;
+
+        public FSM(TState defaultState, TContext context) : base(defaultState)
+        {
+            ConfigureStateMachine();
+            Context = context;
+        }
+
+        public void Tick()
+        {
+            if (!_initialized)
+            {
+                EnterState(State);
+                _initialized = true;
+            }
+
+            _executingState.Tick();
+        }
+
+        protected abstract void ConfigureStateMachine();
+
+        private void EnterState(TState state)
+        {
+            _executingState = (IState)Activator.CreateInstance(_states[state], this);
+            _executingState.OnStateEnter();
+        }
+
+        private void ExitState()
+        {
+            _executingState.OnStateExit();
+        }
+
+        public void SetDefaultState(TState state)
+        {
+            EnterState(state);
+        }
+
+        public StateConfiguration AddState(TState state, Type stateType)
+        {
+            if (!_states.ContainsKey(state))
+                _states.Add(state, stateType);
+
+            return Configure(state).OnEntry(() => EnterState(state)).OnExit(() => ExitState());
+        }
+
+        public void AddGlobalPermit(TTrigger trigger, TState destinationState)
+        {
+            foreach (TState state in Enum.GetValues(typeof(TState)))
+            {
+                if (state.Equals(destinationState))
+                    Configure(state).Ignore(trigger);
+                else
+                    Configure(state).Permit(trigger, destinationState);
+            }
+        }
+    }
+}
